1e9e9bee1b04e7f64a57d6be0f91a3c6e8fc38b3
natalyamyunster <natalyamyunster@yahoo.com>
2021-03-16 13:28:25 +0100

    promises need to be fixed

--- script.js
+++ script.js
@@ -4,8 +4,15 @@ const url = 'https://api.github.com/users/';
 const urlParams = new URLSearchParams(window.location.search);
 const nameFromUrl = urlParams.get('username');
 const userName = nameFromUrl ? nameFromUrl : 'Natalina27';
+const preloader = document.getElementById('preloader');
 
-const createUser = (name, bio, img, url) => {
+const date = new Date();
+const currentDate = `${date.getDate()}/${date.getMonth() +1}/${date.getFullYear()}`;
+
+//helpers
+const createUser = (name, bio, img, url, date) => {
+    const modifDate = date.split('-');
+
     const wrapperDiv = document.getElementById('wrapper');
     const title = document.createElement('h1');
     title.innerText = name;
@@ -13,15 +20,27 @@ const createUser = (name, bio, img, url) => {
     title.classList.add('name');
     wrapperDiv.append(title);
 
-    const paragraph = document.createElement('p');
-    paragraph.innerText = bio;
-    wrapperDiv.append(paragraph);
+    const joined = document.createElement('h3');
+    joined.innerText = `Joined 01/${modifDate[1]}/${modifDate[0]} `;
+    joined.classList.add('date');
+    wrapperDiv.append(joined);
+
+    const bioData = document.createElement('p');
+    bioData.innerText = bio;
+    wrapperDiv.append(bioData);
+
+    const current = document.createElement('h3');
+    current.innerText = `Today :   ${currentDate} `;
+    current.classList.add('current');
+    bioData.append(current);
 
     const div = document.createElement('div');
     const userPic = new Image();
     userPic.src = img;
     wrapperDiv.append(div);
     div.append(userPic);
+
+
 };
 const errorMessage = error =>{
     const wrapperDiv = document.getElementById('wrapper');
@@ -31,22 +50,35 @@ const errorMessage = error =>{
     wrapperDiv.append(title);
 }
 
-setTimeout(() => {
-    const preloader = document.getElementById('preloader');
-    preloader.classList.add('stop');
-}, 2000);
+//promises
+const getName = new Promise((res, rej) =>{
+    setTimeout(() => userName ? res(userName) : rej('Имя не найдено'), 1000)
+});
+const getUrl = new Promise((res, rej) =>{
+    setTimeout(() => date ? res(date) : rej('Ссылка не найдена'), 1000)
+});
+const getDate = new Promise((res, rej) =>{
+    setTimeout(() => url ? res(url) : rej('Дата не найдена'), 1000)
+});
 
-fetch(`${url}${userName}`)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(json => {
-        console.log(json);
-        const { name, bio, avatar_url , html_url} = json;
-        createUser(name, bio, avatar_url, html_url);
+Promise.all([getUrl, getName, getDate])
+    .then(([url, userName]) => fetch(`${url}${userName}`)
+        .then(response => {
+            if (!response.ok) {
+                preloader.classList.add('stop');
+                throw new Error('Network response was not ok');
+            }else{
+                preloader.classList.add('stop');
+                return response.json();
+            }
+        })
+        .then(json => {
+            console.log(json);
+            const { name, bio, avatar_url , html_url, created_at} = json;
+            createUser(name, bio, avatar_url, html_url, created_at);
 
-    })
-    .catch(error => errorMessage(error));
+        })
+        .catch(error => errorMessage(error)));
+
+
+
