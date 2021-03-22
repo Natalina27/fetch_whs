ebd36530996b2537b10dc3a2bea4d298975615aa
natalyamyunster <natalyamyunster@yahoo.com>
2021-03-22 21:01:00 +0100

    21.03.2021 fetch shop

--- script.js
+++ script.js
@@ -1,54 +1,15 @@
-const url = 'https://api.github.com/users/';
+const url = 'https://groupprice.ru/categories/jenskaya-odejda?referer_from=main_catalog';
 
-//Take value from query parameter
-const urlParams = new URLSearchParams(window.location.search);
-const nameFromUrl = urlParams.get('username');
-console.log('nameFromUrl', nameFromUrl);//null
-const userName = nameFromUrl ? nameFromUrl : 'Natalina27';
 
-const createUser = (name, bio, img, url) => {
-    const wrapperDiv = document.getElementById('wrapper');
-    const title = document.createElement('h1');
-    title.innerText = name;
-    title.addEventListener('click', () => window.location = url);
-    title.classList.add('name');
-    wrapperDiv.append(title);
-
-    const paragraph = document.createElement('p');
-    paragraph.innerText = bio;
-    wrapperDiv.append(paragraph);
-
-    const div = document.createElement('div');
-    const userPic = new Image();
-    userPic.src = img;
-    wrapperDiv.append(div);
-    div.append(userPic);
-};
-const errorMessage = error =>{
-    const wrapperDiv = document.getElementById('wrapper');
-    const title = document.createElement('h1');
-    title.innerText = ` ${error.message}`;
-    title.classList.add('error');
-    wrapperDiv.append(title);
-}
-
-setTimeout(() => {
-    const preloader = document.getElementById('preloader');
-    preloader.classList.add('stop');
-}, 2000);
-
-fetch(`${url}${userName}`)
-    .then(response => {
-        console.log('response', response);
-        if (!response.ok) {
-            throw new Error('Информация о пользователе не доступна');
-        }
-        return response.json();
-    })
-    .then(json => {
-        console.log(json);
-        const { name, bio, avatar_url , html_url} = json;
-        createUser(name, bio, avatar_url, html_url);
-
-    })
-    .catch(error => errorMessage(error));
+fetch(url)
+    .then(response => response.text())
+    .then(data => {
+            const doc = new DOMParser().parseFromString(data, 'text/html');
+            console.log('doc',doc);
+            const ourData = Array.from(doc.querySelectorAll('.product-item[data-id]'));
+            console.log('ourData', ourData);
+            const array = ourData.map((el) => el.getAttribute('data-id'));
+            console.log('array', array);
+    }
+        )
+    .catch(error => console.log(error));
