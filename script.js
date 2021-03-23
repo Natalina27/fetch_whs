eb2ed8a8e86fe0e00628eae687591443e825ab8a
natalyamyunster <natalyamyunster@yahoo.com>
2021-03-23 10:36:59 +0100

    23.03.2021 fetch shop

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-const url = 'https://groupprice.ru/categories/jenskaya-odejda?referer_from=main_catalog';
+// const url = 'http://nir-vanna.ru/product/smesitel-bravat-art-f175109c-dlya-rakoviny/';
+//
+// fetch(url)
+//     .then(response => response.text())
+//     .then(data => console.log('data', data))
+//             // const doc = new DOMParser().parseFromString(data, 'text/html');
+//             // console.log('doc',doc);
+//             // const ourData = Array.from(doc.querySelectorAll('.product-item[data-id]'));
+//             // console.log('ourData', ourData);
+//             // const array = ourData.map((el) => el.getAttribute('data-id'));
+//             // console.log('array', array);
+//     .catch(error => console.log(error));
 
+// fetch(url, {mode: 'cors', credentials: "include"})
+//     .then(function(response) {
+//         console.log(response);
+//     }).catch(function(error) {
+//     console.log('Request failed', error)
+// });
 
-fetch(url)
-    .then(response => response.text())
-    .then(data => {
-            const doc = new DOMParser().parseFromString(data, 'text/html');
-            console.log('doc',doc);
-            const ourData = Array.from(doc.querySelectorAll('.product-item[data-id]'));
-            console.log('ourData', ourData);
-            const array = ourData.map((el) => el.getAttribute('data-id'));
-            console.log('array', array);
-    }
-        )
-    .catch(error => console.log(error));
