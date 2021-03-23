bda1ef9f55d274f451cbe94a2638eaaf404f6132
natalyamyunster <natalyamyunster@yahoo.com>
2021-03-23 11:02:38 +0100

    23.03.2021 fetch shop

--- script.js
+++ script.js
@@ -1,54 +1,10 @@
-const url = 'https://api.github.com/users/';
-
-//Take value from query parameter
-const urlParams = new URLSearchParams(window.location.search);
-const nameFromUrl = urlParams.get('username');
-console.log('nameFromUrl', nameFromUrl);//null
-const userName = nameFromUrl ? nameFromUrl : 'Natalina27';
-
-const createUser = (name, bio, img, url) => {
-    const wrapperDiv = document.getElementById('wrapper');
-    const title = document.createElement('h1');
-    title.innerText = name;
-    title.addEventListener('click', () => window.location = url);
-    title.classList.add('name');
-    wrapperDiv.append(title);
-
-    const paragraph = document.createElement('p');
-    paragraph.innerText = bio;
-    wrapperDiv.append(paragraph);
-
-    const div = document.createElement('div');
-    const userPic = new Image();
-    userPic.src = img;
-    wrapperDiv.append(div);
-    div.append(userPic);
-};
-const errorMessage = error =>{
-    const wrapperDiv = document.getElementById('wrapper');
-    const title = document.createElement('h1');
-    title.innerText = ` ${error.message}`;
-    title.classList.add('error');
-    wrapperDiv.append(title);
-}
-
-setTimeout(() => {
-    const preloader = document.getElementById('preloader');
-    preloader.classList.add('stop');
-}, 2000);
-
-fetch(`${url}${userName}`)
-    .then(response => {
-        console.log('response', response);
-        if (!response.ok) {
-            throw new Error('Информация о пользователе не доступна');
-        }
-        return response.json();
+fetch('https://api.github.com/users/6thSence')
+    .then(res=>res.json())
+    .then(json=>{
+            console.log(json);
+            const photo = json.avatar_url;
+            console.log(photo);
+            let img = document.createElement('img');
+            img.src = photo;
+            document.body.append(img);
     })
-    .then(json => {
-        console.log(json);
-        const { name, bio, avatar_url , html_url} = json;
-        createUser(name, bio, avatar_url, html_url);
-
-    })
-    .catch(error => errorMessage(error));
