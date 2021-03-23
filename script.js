8484f5b3be0f988daa2c757da98763106131a145
natalyamyunster <natalyamyunster@yahoo.com>
2021-03-23 19:09:55 +0100

    24.03.2021 fetch shop

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-// const url = 'http://nir-vanna.ru/product/smesitel-bravat-art-f175109c-dlya-rakoviny/';
-//
-// fetch(url)
+const url = 'http://nir-vanna.ru/product/smesitel-bravat-art-f175109c-dlya-rakoviny/';
+const init = {
+    mode: 'cors',
+    headers: {
+        'Access-Control-Allow-Origin':'*'
+    }};
+
+fetch(url, init)
+    .then(response => response.json())
+    .then(data => console.log('data', data))
+    .catch(error => console.log(error));
+
+// fetch(URL, {
+//     mode: 'cors',
+//     headers: {
+//         'Access-Control-Allow-Origin':'*'
+//     }
+// })
+/// fetch(url)
 //     .then(response => response.text())
 //     .then(data => console.log('data', data))
 //             // const doc = new DOMParser().parseFromString(data, 'text/html');
@@ -10,11 +26,4 @@
 //             // const array = ourData.map((el) => el.getAttribute('data-id'));
 //             // console.log('array', array);
 //     .catch(error => console.log(error));
-
-// fetch(url, {mode: 'cors', credentials: "include"})
-//     .then(function(response) {
-//         console.log(response);
-//     }).catch(function(error) {
-//     console.log('Request failed', error)
-// });
-
+//     .then(response => response.json())
