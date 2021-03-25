f7612b3a2e6baaacaa3753df374d57c55f279138
natalyamyunster <natalyamyunster@yahoo.com>
2021-03-25 09:19:33 +0100

    24.03.2021 fetch promo

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-const url = 'https://groupprice.ru/categories/jenskaya-odejda?referer_from=main_catalog';
+const promoLink = document.getElementById('promo-link');
+const url = 'https://httpbin.org/status/200';
 
-
-fetch(url)
-    .then(response => response.text())
-    .then(data => {
-            const doc = new DOMParser().parseFromString(data, 'text/html');
-            console.log('doc',doc);
-            const ourData = Array.from(doc.querySelectorAll('.product-item[data-id]'));
-            console.log('ourData', ourData);
-            const array = ourData.map((el) => el.getAttribute('data-id'));
-            console.log('array', array);
+promoLink.addEventListener('click', async () => {
+    try {
+        await fetch(url);
+    } catch (err) {
+        alert(err);
     }
-        )
-    .catch(error => console.log(error));
+});
